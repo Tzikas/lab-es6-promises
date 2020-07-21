de42be6c3571c7a7482dbd32e6f484f9a818b324
Tzikas <niko.tzikas@gmail.com>
2020-07-21 15:50:08 -0400

    promise lab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,29 +3,106 @@
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
-  console.log(mashPotatoes[i])
+  //console.log(mashPotatoes[i])
 }
 
+
+
+
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
-  addFood(steak[1], '#steak', () => {
+// addFood(steak[0], '#steak', () => {
+//   addFood(steak[1], '#steak', () => {
+//     addFood(steak[2], '#steak', () => {
+//       addFood(steak[3], '#steak', () => {
+//         addFood(steak[4], '#steak', () => {
+//           addFood(steak[5], '#steak', () => {
+//             addFood(steak[6], '#steak', () => { //rest for 10 minutes
+//               addFood(steak[7], '#steak', () => { //enjoy
+//                 document.querySelector('#table').innerHTML += `<img src ="./public/images/steak.jpg" />`
+//               })
+//             })
+//           })
+//         })
+//       })
+//     })
+//   })
+// });
 
-  })
-});
+
+//Nice try but because things have different speeds its out of order 
+// for(let i =0; i<steak.length; i++){
+//   addFood(steak[i], '#steak')
+// }
+
+// let p1 = new Promise((resolve, reject)=>{
+//   function recursiveAddFood(n){
+//     addFood(steak[n], '#steak', () => {
+//       if(n < steak.length - 1){
+//         recursiveAddFood(++n)
+//       } else {
+//         document.querySelector('#table').innerHTML += `<img src ="./public/images/steak.jpg" />`
+//         resolve('steak done')
+//       }
+//     })
+//   }
+//   recursiveAddFood(0)
+// })
+
+// Promise.all([p1]).then(res => {
+//   console.log(res)
+// })
 
 
 
-// Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
-});
+
+
+
+
+
+// addFood(mashPotatoes[0], '#mashPotatoes').then((res)=>{
+//   addFood(mashPotatoes[1], '#mashPotatoes').then((res)=>{
+//     addFood(mashPotatoes[2], '#mashPotatoes').then((res)=>{
+//       addFood(mashPotatoes[3], '#mashPotatoes').then((res)=>{
+//         addFood(mashPotatoes[4], '#mashPotatoes').then((res)=>{
+//           document.querySelector('#table').innerHTML += `<img src ="./public/images/mashPotatoes.jpg" />`
+//         })
+//       })
+//     })
+//   })
+// })
+
+
+// function recursiveAddFoodmashPotatoes(n){
+//   addFood(mashPotatoes[n], '#mashPotatoes').then(() => {
+//     if(n < mashPotatoes.length - 1){
+//       recursiveAddFoodmashPotatoes(++n)
+//     }
+//     else {
+//       document.querySelector('#table').innerHTML += `<img src ="./public/images/mashPotatoes.jpg" />`
+//     }
+//   })
+// }
+// recursiveAddFoodmashPotatoes(0)
+
+
+
+
 
 // Iteration 3 using async and await
-
-  async function makeFood(step) {
-    // ... your code here
-    
+  async function makeFood(steps, id) {
+    console.log(steps)
+    for(step of steps){       //for(let i=0; i< mashPotatoes.length; i++){
+      await addFood(step, id)     //await addFood(mashPotatoes[i], '#mashPotatoes)
+    }
+    document.querySelector('#table').innerHTML += `<img src ="./public/images/${id.replace('#','')}.jpg" />`
+    return steps
   }
-  makeFood(eachStep);
+
+  Promise.all([
+    makeFood(brusselSprouts, '#brusselSprouts'),
+    makeFood(mashPotatoes, '#mashPotatoes'),
+    makeFood(steak, '#steak')
+  ]).then(res => { //All done
+    console.log('Dinner is made', res)
+    document.body.innerHTML += `<button onclick="new Audio('./public/media/dinnerIsServed.mp3').play()">Dinner is served</button>`
+  })
